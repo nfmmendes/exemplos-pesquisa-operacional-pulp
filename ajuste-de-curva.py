6c9b0685ac828d049399b35b677c6df2bfb94a18
nfmmendes <nilson.fmmendes@outlook.com>
2024-07-19 20:47:25 +0200

    Add comments to example

--- ajuste-de-curva.py
+++ ajuste-de-curva.py
@@ -1,13 +1,17 @@
 import pulp as lp
 import pandas as pd
 
+## Pontos observados
 df = pd.DataFrame(data=[[0, 1, 2, 3], [-1, 0, 1, 1]], index=["x", "y"])
 
+## Mostra o dataframe 
 print(df)
 
+## Variaveis para os coeficientes das retas.
 a = lp.LpVariable(name="a")
 b = lp.LpVariable(name="b")
 
+## Variaveis (positivas) para os erros.
 variaveis_erro = []
 for i in range(0, len(df.columns)):
     variaveis_erro.append(
@@ -17,23 +21,29 @@ for i in range(0, len(df.columns)):
         }
     )
 
+## Cria um modelo
 model = lp.LpProblem()
 
+## Funcao objetivo
 model += lp.lpSum(
     variaveis_erro[i][sinal] for i in range(0, len(df.columns)) for sinal in ["+", "-"]
 )
 
+## Restricoes
 for i in range(0, len(df.columns)):
     model += (
         a * df[i]["x"] + b + variaveis_erro[i]["+"] - variaveis_erro[i]["-"]
         == df[i]["y"]
     )
 
+## Imprime o modelo
 print(model)
 
+## Resolve o problema
 model.solve()
 
 print("Status: ", lp.LpStatus[model.status])
 
+# Imprime a solucao
 for v in model.variables():
     print(v.name, " = ", v.varValue)
